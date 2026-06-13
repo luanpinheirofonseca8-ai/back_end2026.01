a29345ceb1d04222df7c1fe0847002be5b3e00ca
luanpinheirofonseca8-ai <luanpinheirofonseca8@gmail.com>
2026-06-12 22:03:34 -0300

    projeto banco

---
+++ banco/banco/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return self.nome
